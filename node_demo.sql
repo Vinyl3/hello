891664311774b1df9a48a85312a2209f4a89aca0
Vinyl3 <pratyush.pk3@gmail.com>
2017-02-01 15:33:04 +0530

    changes

--- node_demo.sql
+++ node_demo.sql
@@ -12,9 +12,9 @@ MySQL - 5.5.50 : Database - node_demo1
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`node_demo1` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`node_demo` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `node_demo1`;
+USE `node_demo`;
 
 /*Table structure for table `companydetails` */
 
